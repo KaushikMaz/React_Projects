ac38092fadf137df8508dd9a1973d56e17ede267
Kaushik <kmzdr1@gmail.com>
2023-03-26 21:27:43 +0530

    Update: Navbar component corrected

--- React_infosite/components/Navbar.js
+++ React_infosite/components/Navbar.js
@@ -1,7 +1,11 @@
 
 const Navbar=()=>{
     return(
-        <h1>Navbar goes here</h1>
+        <nav>
+            <img src="/images/react-logo.png" alt= "React Logo" className="logo"/>
+            <h3 className="logoText">React Facts</h3>
+            <h4 className="navTitle">Information Site</h4>
+        </nav>
     )
 
 } 
