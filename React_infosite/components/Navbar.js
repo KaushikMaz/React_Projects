8d0971bf0d75ffb156b1b17a8831a21de9b8c5b5
Kaushik <kmzdr1@gmail.com>
2023-04-22 14:54:26 +0530

    update: Styling of the body & added logo in a separate file

--- React_infosite/components/Navbar.js
+++ React_infosite/components/Navbar.js
@@ -1,8 +1,9 @@
+import logo from "/images/react-logo.png"
 
 const Navbar=()=>{
     return(
         <nav>
-            <img src="/images/react-logo.png" alt= "React Logo" className="logo"/>
+            <img src={logo} alt= "React Logo" className="logo"/>
             <h3 className="logoText">React Facts</h3>
             <h4 className="navTitle">Information Site</h4>
         </nav>
